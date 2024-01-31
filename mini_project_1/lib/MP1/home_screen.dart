20ceb2718a50a7be39a160afd57b2e423eccf34e
amalrivel <amalrivel@gmail.com>
2024-01-31 19:17:20 +0700

    feat: membuat halaman home

--- mini_project_1/lib/MP1/home_screen.dart
+++ mini_project_1/lib/MP1/home_screen.dart
@@ -1,10 +1,97 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:mini_project_1/MP1/models/operator_model.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: Stack(children: [
+        Container(
+          height: height * 0.36,
+          width: width,
+          padding: const EdgeInsets.all(24.0),
+          child: Stack(children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                IconButton(
+                    icon: Icon(Icons.account_circle),
+                    onPressed: () => print('Tombol Profile Ditekan')),
+              ],
+            ),
+            Center(
+              child: Image.network(
+                  'https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Arknights_English_Release_Logo.svg/1280px-Arknights_English_Release_Logo.svg.png'),
+            ),
+          ]),
+        ),
+        DraggableScrollableSheet(
+          initialChildSize: 0.7,
+          minChildSize: 0.7,
+          maxChildSize: 1.0,
+          builder: (BuildContext context, scrollController) {
+            return ClipRRect(
+              borderRadius: BorderRadius.vertical(top: Radius.circular(30.0)),
+              child: Container(
+                padding: const EdgeInsets.all(24.0),
+                color: Colors.black54,
+                child: GridView.count(
+                  controller: scrollController,
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 16,
+                  children: operatorModelList
+                      .map((e) => Card(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              children: [
+                                Expanded(
+                                  child: Image.network(
+                                    e.image,
+                                    fit: BoxFit.cover,
+                                    errorBuilder: (context, error, stackTrace) {
+                                      if (kDebugMode) {
+                                        print('Error loading image: $error');
+                                      }
+                                      return ErrorWidget(
+                                          'Image failed to load');
+                                    },
+                                  ),
+                                ),
+                                const SizedBox(height: 8),
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 8.0),
+                                  child: Text(
+                                    e.name,
+                                    style: const TextStyle(
+                                      fontSize: 16.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                      left: 8.0, bottom: 8.0),
+                                  child: Text(
+                                    e.classType.join(', '),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ))
+                      .toList(),
+                ),
+              ),
+            );
+          },
+        ),
+      ]),
+    );
   }
 }
